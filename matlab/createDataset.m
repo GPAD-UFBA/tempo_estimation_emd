d341a5f33ca1ba4536d5e95799159fcf478f5c8e
unknown <caleomenesessantos@gmail.com>
2022-06-05 22:06:54 -0300

    data folder added

--- matlab/createDataset.m
+++
@@ -1,7 +0,0 @@
-function [] = createDataset(datasets_list)
-    datasets_list_length = length(datasets_list);
-    for i=3:datasets_list_length
-        name_dataset = strcat('/',datasets_list(i).name);
-        h5create('dataset.h5',name_dataset,[inf inf],'ChunkSize',[1 11025])
-    end
-end
